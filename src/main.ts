cdf089b5649f75397de378a853578c234653190a
Elena <elen.vasileva@gmail.com>
2024-03-11 21:12:04 +0100

    swagger & .env file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder().setTitle('REST service').setDescription('The REST service API description').setVersion('1.0').addTag('library').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  dotenv.config();
+  const port = process.env.PORT;
+
+  await app.listen(port);
 }
 bootstrap();
